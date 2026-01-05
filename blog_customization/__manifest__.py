3a5c08ed1922306cb4c47e237b9e59f079e70186
Kashif Aziz <kashifaziz@kashifazizs-MacBook-Air.local>
2026-01-05 16:18:51 +0500

    blog customization

---
+++ blog_customization/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Blog Customization',
+    'version': '16.0.1.0',
+    'summary': 'Adds custom card image field to blog posts',
+    'depends': ['website_blog'],
+    'data': [
+        'views/blog_post_views.xml',
+    ],
+
+    'installable': True,
+}
